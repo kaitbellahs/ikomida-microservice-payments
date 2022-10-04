7cfa3b9ca1d0ba66ba3859413b59a617fb212250
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-10-04 11:54:34 -0300

    Add workflow
    remove unused configuration files
    Update Dockerfile
    Update rollup configuration
    format code

--- src/controllers/Vendor.ts
+++ src/controllers/Vendor.ts
@@ -1,24 +1,24 @@
-import { Domain, Utils, BackendTypes, Logics, Types, DBModels } from '@ikomida/shared-backend';
+import { Domain, Utils, BackendTypes, Logics, Types, DBModels } from '@ikomida/shared-backend'
 
 export default class Vendor {
-  logger;
-  limit = 10;
+  logger
+  limit = 10
 
   constructor(logger: Utils.Logger) {
-    this.logger = logger;
+    this.logger = logger
   }
 
   async newCoupon(identity: Types.Classes.CUser, input: any) {
     try {
       const coupon: Types.Classes.CCoupon = Types.Classes.CCoupon.fromObject(input)
-      const role = BackendTypes.Roles.valueOf(identity.role);
+      const role = BackendTypes.Roles.valueOf(identity.role)
       if (!coupon.validate() || !role || ![BackendTypes.Roles.VENDOR, BackendTypes.Roles.STAFF].includes(role)) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_COUPON_VENDOR);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_COUPON_VENDOR)
+        return error.logAndReturn(this.logger)
       }
       const contractModel = await DBModels.ContractModel.findOne({
         where: {
-          ikomidaID: identity.ikomidaID,
+          ikomidaID: identity.ikomidaID
         },
         include: [
           {
@@ -27,57 +27,60 @@ export default class Vendor {
             where: {
               id: identity.id,
               role: {
-                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR, BackendTypes.Roles.STAFF],
-              },
-            },
+                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR, BackendTypes.Roles.STAFF]
+              }
+            }
           },
           {
             model: DBModels.VendorSettingsModel,
-            required: false,
+            required: false
           },
           {
             model: DBModels.PlanModel,
-            required: false,
+            required: false
           },
           {
             model: DBModels.CouponModel,
-            required: false,
-          },
-        ],
-      });
+            required: false
+          }
+        ]
+      })
       if (!contractModel) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_COUPON_INVALID_CONTRACT);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_COUPON_INVALID_CONTRACT)
+        return error.logAndReturn(this.logger)
       }
-      const couponsLimit = contractModel?.plan?.coupons ?? -1;
+      const couponsLimit = contractModel?.plan?.coupons ?? -1
       if (couponsLimit !== 0 && (contractModel?.coupons?.length ?? 0) >= couponsLimit) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_COUPON_LIMIT_EXCEEDED, couponsLimit);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(
+          Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_COUPON_LIMIT_EXCEEDED,
+          couponsLimit
+        )
+        return error.logAndReturn(this.logger)
       }
       await contractModel.$create('coupon', {
         name: coupon.name,
         validity: coupon.validity,
         value: Logics.Finances.toFinanceNumber(coupon.value),
         valueType: coupon.valueType,
-        quantity: coupon.quantity ? coupon.quantity : 0,
-      });
-      return new Utils.Return(true);
+        quantity: coupon.quantity ? coupon.quantity : 0
+      })
+      return new Utils.Return(true)
     } catch (e) {
-      this.logger.error(e);
+      this.logger.error(e)
     }
-    return new Utils.Return(false);
+    return new Utils.Return(false)
   }
 
   async removeCoupon(identity: Types.Classes.CUser, id?: string) {
     try {
-      const role = BackendTypes.Roles.valueOf(identity.role);
+      const role = BackendTypes.Roles.valueOf(identity.role)
       if (!role || ![BackendTypes.Roles.VENDOR, BackendTypes.Roles.ADMIN].includes(role)) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_REMOVE_COUPON_VENDOR);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_REMOVE_COUPON_VENDOR)
+        return error.logAndReturn(this.logger)
       }
       const contractModel = await DBModels.ContractModel.findOne({
         where: {
-          ikomidaID: identity.ikomidaID,
+          ikomidaID: identity.ikomidaID
         },
         include: [
           {
@@ -86,54 +89,54 @@ export default class Vendor {
             where: {
               id: identity.id,
               role: {
-                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR],
-              },
-            },
+                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR]
+              }
+            }
           },
           {
             model: DBModels.CouponModel,
             required: false,
             where: {
-              id,
-            },
-          },
-        ],
-      });
+              id
+            }
+          }
+        ]
+      })
       if (!contractModel) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_REMOVE_COUPON_INVALID_CONTRACT);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_REMOVE_COUPON_INVALID_CONTRACT)
+        return error.logAndReturn(this.logger)
       }
-      const couponModels = contractModel?.coupons;
+      const couponModels = contractModel?.coupons
       if (!couponModels || couponModels.length !== 1) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_REMOVE_COUPON_COUPON_NOT_FOUND);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_REMOVE_COUPON_COUPON_NOT_FOUND)
+        return error.logAndReturn(this.logger)
       }
-      await couponModels[0].destroy();
-      return new Utils.Return(true);
+      await couponModels[0].destroy()
+      return new Utils.Return(true)
     } catch (exception: any) {
-      this.logger.error(exception);
-      return new Utils.Return(false);
+      this.logger.error(exception)
+      return new Utils.Return(false)
     }
   }
 
   async getCoupons(identity: Types.Classes.CUser, timestamp = 0) {
     try {
-      const role = BackendTypes.Roles.valueOf(identity.role);
+      const role = BackendTypes.Roles.valueOf(identity.role)
       if (role !== BackendTypes.Roles.VENDOR) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_GET_COUPON_VENDOR);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_GET_COUPON_VENDOR)
+        return error.logAndReturn(this.logger)
       }
       const where =
         timestamp && timestamp != 0 && Number(Logics.Finances.toNumber(timestamp)) == timestamp
           ? {
-            createdAt: {
-              [Domain.SqlDB.Op.lt]: new Date(Number(Logics.Finances.toNumber(timestamp))),
-            },
-          }
-          : null;
+              createdAt: {
+                [Domain.SqlDB.Op.lt]: new Date(Number(Logics.Finances.toNumber(timestamp)))
+              }
+            }
+          : null
       const contractModel = await DBModels.ContractModel.findOne({
         where: {
-          ikomidaID: identity.ikomidaID,
+          ikomidaID: identity.ikomidaID
         },
         include: [
           {
@@ -142,29 +145,29 @@ export default class Vendor {
             where: {
               id: identity.id,
               role: {
-                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR],
-              },
-            },
+                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR]
+              }
+            }
           },
           {
             model: DBModels.CouponModel,
             required: false,
             where: {
               ...{},
-              ...where,
+              ...where
             },
             order: [['createdAt', 'DESC']],
-            limit: this.limit,
-          },
-        ],
-      });
+            limit: this.limit
+          }
+        ]
+      })
       if (!contractModel) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_GET_COUPON_INVALID_CONTRACT);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_GET_COUPON_INVALID_CONTRACT)
+        return error.logAndReturn(this.logger)
       }
-      const couponModels = contractModel?.coupons;
+      const couponModels = contractModel?.coupons
 
-      const coupons = couponModels?.map((couponModel) => {
+      const coupons = couponModels?.map(couponModel => {
         return Types.Classes.CCoupon.init(
           couponModel.name ?? '-',
           couponModel.value ?? 0,
@@ -173,27 +176,27 @@ export default class Vendor {
           couponModel.validity,
           couponModel?.createdAt,
           couponModel.id,
-          couponModel?.createdAt.getTime(),
-        );
-      });
+          couponModel?.createdAt.getTime()
+        )
+      })
       return new Utils.Return(
         true,
-        coupons?.sort((item1, item2) => (item2?.timestamp ?? 0) - (item1?.timestamp ?? 0)),
-      );
+        coupons?.sort((item1, item2) => (item2?.timestamp ?? 0) - (item1?.timestamp ?? 0))
+      )
     } catch (exception: any) {
-      this.logger.error(exception);
-      return new Utils.Return(false);
+      this.logger.error(exception)
+      return new Utils.Return(false)
     }
   }
 
   async getCouponsCount(identity: Types.Classes.CUser) {
-    const role = BackendTypes.Roles.valueOf(identity.role);
+    const role = BackendTypes.Roles.valueOf(identity.role)
     if (role !== BackendTypes.Roles.VENDOR) {
-      return new Utils.Return(false, 0);
+      return new Utils.Return(false, 0)
     }
     const contractModel = await DBModels.ContractModel.findOne({
       where: {
-        ikomidaID: identity.ikomidaID,
+        ikomidaID: identity.ikomidaID
       },
       include: [
         {
@@ -202,29 +205,31 @@ export default class Vendor {
           where: {
             id: identity.id,
             role: {
-              [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR],
-            },
-          },
+              [Domain.SqlDB.Op.in]: [BackendTypes.Roles.ADMIN, BackendTypes.Roles.VENDOR]
+            }
+          }
         },
         {
           model: DBModels.CouponModel,
-          required: false,
-        },
-      ],
-    });
+          required: false
+        }
+      ]
+    })
     if (!contractModel) {
-      const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_GET_COUPONS_COUNT_INVALID_CONTRACT);
-      return error.logAndReturn(this.logger);
+      const error = new Utils.iKomidaError(
+        Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_GET_COUPONS_COUNT_INVALID_CONTRACT
+      )
+      return error.logAndReturn(this.logger)
     }
-    const couponModels = contractModel?.coupons;
-    return new Utils.Return(true, couponModels?.length);
+    const couponModels = contractModel?.coupons
+    return new Utils.Return(true, couponModels?.length)
   }
 
   async getSubscription(identity: Types.Classes.CUser) {
     try {
       const contractModel = await DBModels.ContractModel.findOne({
         where: {
-          ikomidaID: identity.ikomidaID,
+          ikomidaID: identity.ikomidaID
         },
         include: [
           {
@@ -233,13 +238,13 @@ export default class Vendor {
             where: {
               id: identity.id,
               role: {
-                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.VENDOR],
-              },
-            },
+                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.VENDOR]
+              }
+            }
           },
           {
             model: DBModels.PlanModel,
-            required: true,
+            required: true
           },
           {
             model: DBModels.ContractPaymentSignatureModel,
@@ -251,36 +256,42 @@ export default class Vendor {
                 order: [['dueDate', 'DESC']],
                 where: {
                   dueDate: {
-                    [Domain.SqlDB.Op.lte]: Logics.DateTime?.parseAsaasDate(Logics.DateTime?.localToday()),
-                  },
-                },
-              },
-            ],
-          },
-        ],
-      });
+                    [Domain.SqlDB.Op.lte]: Logics.DateTime?.parseAsaasDate(Logics.DateTime?.localToday())
+                  }
+                }
+              }
+            ]
+          }
+        ]
+      })
       if (!contractModel) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_CONTRACT);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(
+          Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_CONTRACT
+        )
+        return error.logAndReturn(this.logger)
       }
-      const contractPaymentSignature = contractModel?.contractPaymentSignature;
+      const contractPaymentSignature = contractModel?.contractPaymentSignature
       if (!contractPaymentSignature) {
         const error = new Utils.iKomidaError(
-          Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_PAYMENT_SIGNATURE,
-        );
-        return error.logAndReturn(this.logger);
+          Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_PAYMENT_SIGNATURE
+        )
+        return error.logAndReturn(this.logger)
       }
-      const planModel = contractModel?.plan;
+      const planModel = contractModel?.plan
       if (!planModel) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_PLAN);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(
+          Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_PLAN
+        )
+        return error.logAndReturn(this.logger)
       }
-      const chargeModels = contractPaymentSignature?.contractPayments;
+      const chargeModels = contractPaymentSignature?.contractPayments
       if (!chargeModels || chargeModels.length === 0) {
-        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_PAYMENTS);
-        return error.logAndReturn(this.logger);
+        const error = new Utils.iKomidaError(
+          Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_INVALID_PAYMENTS
+        )
+        return error.logAndReturn(this.logger)
       }
-      const charges = chargeModels?.map((charge) => {
+      const charges = chargeModels?.map(charge => {
         return Types.Classes.CSubscriptionCharge.init(
           charge?.value ?? 0,
           charge?.creditCardNumber ?? 0,
@@ -289,22 +300,25 @@ export default class Vendor {
           charge?.status ?? Types.Types.TAsaasPaymentStatus.PENDING,
           charge?.invoiceUrl ?? undefined,
           charge?.transactionReceiptUrl ?? undefined,
-          charge?.confirmedDate ?? undefined,
-        );
-      });
+          charge?.confirmedDate ?? undefined
+        )
+      })
       const subscriptionObject = Types.Classes.CSubscription.init(
         planModel?.name ?? '-',
         contractPaymentSignature?.value ?? 0,
         contractPaymentSignature?.createdAt,
         contractPaymentSignature?.status ?? Types.Types.TAsaasSignatureStatus.CANCELED,
         contractPaymentSignature?.nextDueDate ?? new Date(),
-        charges,
-      );
-      return new Utils.Return(true, subscriptionObject);
+        charges
+      )
+      return new Utils.Return(true, subscriptionObject)
     } catch (exception: any) {
-      this.logger.warn(exception);
-      const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_EXCEPTION, exception);
-      return error.logAndReturn(this.logger);
+      this.logger.warn(exception)
+      const error = new Utils.iKomidaError(
+        Utils.iKomidaError.IKOMIDA_PAYMENTS_SERVICE_NEW_PAYMENT_METHOD_EXCEPTION,
+        exception
+      )
+      return error.logAndReturn(this.logger)
     }
   }
 }
